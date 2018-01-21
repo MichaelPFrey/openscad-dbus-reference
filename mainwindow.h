bcfa03f58e6c6e6c453e9924c839f6c252c6a231
Michael Frey <michael.frey@gmx.ch>
2018-01-21 11:05:39 +0100

    initizial commit

---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "openscad_interface.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButtonRotate_clicked();
+
+    void on_pushButtonRotateTo_clicked();
+
+    void on_pushButtonTranslate_clicked();
+
+    void on_pushButtonTranslateTo_clicked();
+
+    void on_pushButtonZoom_clicked();
+
+    void on_pushButtonZoomTo_clicked();
+
+protected:
+    void timerEvent(QTimerEvent *event);
+
+private:
+    Ui::MainWindow *ui;
+    org::openscad::OpenSCAD* OpenSCAD;
+};
+
+#endif // MAINWINDOW_H
